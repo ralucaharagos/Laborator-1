736c5392e42157047a87f061e7003c4608ccc61b
Raluca Costi <costi.bleotu@gmail.com>
2011-10-05 17:15:16 +0300

    final

--- test.pl
+++ test.pl
@@ -1,1 +1,21 @@
-print "Primul program Perl\n";
+use warnings;
+#$a=<STDIN>;
+#$b="ceva";
+#$c=3.4;
+#@vector=(1,"ceva",3,4);
+#$a=scalar@vector;
+#print "$vector[1]   nr elemente: $a\n";
+#%zile =("Luni"=> 1,"marti"=> 2,"Miercuri"=> 3);
+#print $zile {"marti"};
+#print keys %zile;
+print "a= "; 
+$a=<STDIN>;
+print "b= "; 
+$b=<STDIN>;
+if($a==$b){
+	print "DA\n";
+	}
+else { 
+	print "NU\n";
+	}
+#print "a=$a, b=$b, c=$c \n";
